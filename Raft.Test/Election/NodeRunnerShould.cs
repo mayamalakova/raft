0d398d4193addc7ef8d4b8230f7526472ff09694
Maya.Malakova <maya.malakova@gmail.com>
2019-07-21 18:20:00 +0100

    add tests for follower commit

--- Raft.Test/Election/NodeRunnerShould.cs
+++ Raft.Test/Election/NodeRunnerShould.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using NSubstitute;
 using NUnit.Framework;
 using Raft.Election;
@@ -7,23 +8,43 @@ using Shouldly;
 
 namespace Raft.Test.Election
 {
-    [TestFixture]
     public class NodeRunnerShould
     {
         private const string TestValue = "test-message";
         
-        [Test]
-        public void UpdateLog_OnLogUpdate()
-        {
-            var messageBroker = Substitute.For<IMessageBroker>();
-            var nodeRunner = new NodeRunner("test", 100, messageBroker);
+        private NodeRunner _nodeRunner;
+        private IMessageBroker _messageBroker;
 
+        [SetUp]
+        public void SetUp()
+        {
+            _messageBroker = Substitute.For<IMessageBroker>();
+            _nodeRunner = new NodeRunner("test", 100, _messageBroker);
+        }
+        
+        [Test]
+        public void UpdateLogAndConfirm_OnLogUpdate()
+        {
             var message = new NodeMessage(TestValue, MessageType.LogUpdate, null, Guid.Empty);
-            nodeRunner.ReceiveMessage(message);
+            _nodeRunner.ReceiveMessage(message);
             
-            nodeRunner.Log.Count.ShouldBe(1);
-            nodeRunner.Log[0].Value.ShouldBe(TestValue);
-            nodeRunner.Log[0].Type.ShouldBe(OperationType.Update);
+            _nodeRunner.Log.Count.ShouldBe(1);
+            _nodeRunner.Log[0].Value.ShouldBe(TestValue);
+            _nodeRunner.Log[0].Type.ShouldBe(OperationType.Update);
+            _messageBroker.Received(1).Broadcast(Arg.Is<NodeMessage>(m => m.Type == MessageType.LogUpdateConfirmation));
+        }
+
+        [Test]
+        public void Commit_OnLogCommit()
+        {
+            var updateId = Guid.NewGuid();
+            _nodeRunner.Log.Add(new LogEntry(OperationType.Update, TestValue, updateId));
+            
+            var message = new NodeMessage(TestValue, MessageType.LogCommit, null, updateId);
+            _nodeRunner.ReceiveMessage(message);
+            
+            _nodeRunner.Log.Last().Type.ShouldBe(OperationType.Commit);
+            _nodeRunner.Node.Value.ShouldBe(TestValue);
         }
     }
 }
