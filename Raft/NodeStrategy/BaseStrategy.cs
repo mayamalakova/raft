f16c64e00cc753076766d0d9ea90b1dc87812085
Maya.Malakova <maya.malakova@gmail.com>
2019-08-30 19:26:50 +0100

    newly elected leaders ping immediately

--- Raft/NodeStrategy/BaseStrategy.cs
+++ Raft/NodeStrategy/BaseStrategy.cs
@@ -14,6 +14,7 @@ namespace Raft.NodeStrategy
             Logger.Debug($"{Node.Name} becomes leader, term: {Node.Status.Term}");
             
             Node.Status = new LeaderStatus(Node.Status.Term);
+            Node.SendPing();
         }
 
         protected void BecomeFollower(NodeMessage message)
@@ -25,8 +26,15 @@ namespace Raft.NodeStrategy
 
         protected void ConfirmLogUpdate(NodeMessage message)
         {
-            Node.UpdateLog(message, message.Id);
-            Node.ConfirmLogUpdate(message.Id);
+            
+                Node.UpdateLog(message, message.Id);
+                Node.ConfirmLogUpdate(message.Id);
+
+        }
+
+        protected void CommitLog(NodeMessage message)
+        {
+            Node.CommitLog(message);
         }
     }
 }
